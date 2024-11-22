f3fa6583608217d353a6a241b7dd16039a58e4a7
Matteo Levy <levymatteo@gmail.com>
2024-11-22 23:59:25 +0100

    add basic pytest for run.py

---
+++ tests/test_run.py
@@ -0,0 +1,7 @@
+from app.run import app
+
+def test_home():
+    # Test the home route
+    with app.test_client() as client:
+        response = client.get("/")
+        assert response.status_code == 200
