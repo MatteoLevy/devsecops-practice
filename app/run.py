d4afe3ea0d8289b7deb633305fab8cfb5f17f37f
Matteo Levy <levymatteo@gmail.com>
2024-11-23 20:04:09 +0100

    test changing github settings for merge

--- app/run.py
+++ app/run.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Welcome to DevSecOps!"
+    return "Welcome to DevSecOps!!!"
 
 if __name__ == "__main__":
     app.run(host="127.0.0.1", port=5000)
